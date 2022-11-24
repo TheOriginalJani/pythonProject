2c6950c8377414c6f665083eeb4479b20e97550f
TheOriginalJani <suskajanos@gmail.com>
2022-11-24 23:22:50 +0100

    Kesz

--- qrCode.py
+++ qrCode.py
@@ -1,1 +1,30 @@
+import qrcode
+from PIL import Image
+from pyzbar.pyzbar import decode
 
+path = ''
+
+
+def encoder():
+    try:
+        file = open(path, 'r')
+        fileData = file.read()
+    finally:
+        file.close()
+    qr = qrcode.QRCode(version=1, box_size=25, border=1)
+    qr.add_data(fileData)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='#4B0082', back_color='#00FF7F')
+    pngName = path[:len(path) - 4] + '.png'
+    img.save(pngName)
+    return pngName
+
+
+def decoder():
+    decoded = decode(Image.open(path))
+    decodedData = decoded[0].data.decode('utf-8')
+    txtName = path[:len(path) - 4] + '.txt'
+    file = open(txtName, 'w', encoding='utf-8')
+    file.write(decodedData)
+    file.close()
+    return txtName
--- userInterface.py
+++ userInterface.py
@@ -1,7 +1,66 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
 
-# Ablak beállításai
+import qrCode
+
+
+def select_file():
+    filetypes = (
+        ('Minden fájl', '*.*'),
+        ('Szöveg fájl', '*.txt'),
+        ('Kép fájl', '*.png')
+    )
+
+    filename = askopenfilename(
+        title='Válassz egy fájlt',
+        initialdir='/',
+        filetypes=filetypes)
+
+    if filename == "":
+        if ntry_fileName["text"] == "":
+            messagebox.showerror(title="Hiba", message="Válassz fájlt!")
+        return
+
+    ntry_fileName.config(state='normal')
+    ntry_fileName.delete(0, END)
+    ntry_fileName.insert(0, filename)
+    ntry_fileName.config(state='disable')
+    fileType(filename)
+
+
+def fileType(path):
+    qrCode.path = path
+    if path.endswith('.txt'):
+        btn_decoder.config(state='disable')
+        btn_encoder.config(state='normal')
+    elif path.endswith('.png'):
+        btn_encoder.config(state='disable')
+        btn_decoder.config(state='normal')
+    else:
+        btn_encoder.config(state='disable')
+        btn_decoder.config(state='disable')
+        messagebox.showerror(title="Hiba", message="Nem megfelelő fájlformátum!")
+
+
+def encodeFile():
+    try:
+        newFilePath = qrCode.encoder()
+        messagebox.showinfo(title="Siker", message=f"Elkészült a QR-kód!\n{newFilePath}")
+    except:
+        messagebox.showerror(title="Hiba", message="Nem sikerült elkészíteni a QR-kódot!")
+
+
+def decodeFile():
+    try:
+        newFilePath = qrCode.decoder()
+        messagebox.showinfo(title="Siker", message=f"Elkészült a szöveges fájl!\n{newFilePath}")
+    except:
+        messagebox.showerror(title="Hiba", message="Nem sikerült elkészíteni a szöveges fájlt!")
+
+
+# Ablak beállítása
 window = tk.Tk()
 window.title('QR code encoder / decoder')
 window.resizable(False, False)
@@ -18,19 +77,20 @@ lbl_emptySpace = Label(
     state="disable")
 
 # 1. sor
-lbl_actualFileLabel = Label(
-    text='Aktuális fájl:',
+lbl_fileNameLabel = Label(
+    text='Fájl neve:',
     width=15
 )
 
-lbl_actualFileName = Entry(
+ntry_fileName = Entry(
     width=30,
     state='disable'
 )
 
 btn_setFile = Button(
     text='Fájl választása',
-    width=15
+    width=15,
+    command=select_file
 )
 
 # 2. sor
@@ -38,30 +98,32 @@ frm_coders = Frame(
     master=window,
 )
 
-btn_encodeFile = tk.Button(
+btn_encoder = tk.Button(
     master=frm_coders,
-    text='Encode',
+    text='Encoder',
     state="disable",
-    width=30
+    width=30,
+    command=encodeFile
 )
 
-btn_decodeFile = tk.Button(
+btn_decoder = tk.Button(
     master=frm_coders,
-    text='Decode',
+    text='Decoder',
     state="disable",
-    width=30
+    width=30,
+    command=decodeFile
 )
 
 lbl_Title.grid(row=0, column=0, padx=5, pady=5, sticky="nsew", columnspan=3)
 
 lbl_emptySpace.grid(row=1, column=0, padx=5, pady=5, sticky="nsew", columnspan=3)
 
-lbl_actualFileLabel.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
-lbl_actualFileName.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
+lbl_fileNameLabel.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+ntry_fileName.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 btn_setFile.grid(row=2, column=2, padx=5, pady=5, sticky="nsew")
 
 frm_coders.grid(row=3, column=0, padx=5, pady=5, columnspan=3)
-btn_encodeFile.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
-btn_decodeFile.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
+btn_encoder.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+btn_decoder.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
 
 window.mainloop()
